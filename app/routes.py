b60bd383f70479f08e1222e6dd36e7748b776e98
Cass Golkin <cassgolkin@gmail.com>
2023-03-08 11:00:47 -0500

    base and flaskenv added

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,18 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Cass'}
+    actions = [
+        {
+            'tech': {'username': 'Steve'},
+            'details': 'Ran computer name check.'
+        },
+        {
+            'tech': {'username': 'Christian'},
+            'details': 'Ran netID check.'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, actions=actions)
