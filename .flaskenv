3050185b0b8d25c61e988ee53bb50f6ce73e9e54
Cass Golkin <cassgolkin@gmail.com>
2023-03-09 10:09:49 -0500

    database added

---
+++ .flaskenv
@@ -0,0 +1,1 @@
+FLASK_APP=absolute_portal.py
---
+++ app/models.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+from app import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+    entries = db.relationship('Log_Entry', backref='tech', lazy='dynamic')
+
+    def __repr__(self):
+        return f'<User {self.username}>'
+
+class Log_Entry(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return f'<Log_Entry {self.body}>'
+
